761b2f03656f05c4fc783c44208a553997077f73
BlueTRaven <Taylor@adoniram.net>
2014-11-04 15:45:14 -0500

    Added csproj stuff
    also ported over my boss npc, Eyerok, and helper.cs

---
+++ Helper.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using TAPI;
+using Terraria;
+
+namespace MysticalMagics
+{
+    class Helper
+    {
+        /*
+         *  Gets the rotation to its target.
+         *  @Param pos: 4 possible ints
+         *  use 0 when charging a target
+         *  using 2 when shooting a projectile at the player
+         */
+        public static float GetRotation(Player target, NPC npc, int pos)
+        {
+            if (pos == 0)
+                return (target.Center - npc.Center).ToRotation() + -MathHelper.PiOver2;
+            else if (pos == 1)
+                return (target.Center - npc.Center).ToRotation() + MathHelper.PiOver2;
+            else if (pos == 2)
+                return (target.Center - npc.Center).ToRotation() + ((float)-Math.PI);
+            else if (pos == 3)
+                return (target.Center - npc.Center).ToRotation() + ((float)Math.PI);
+            
+            else return (target.Center - npc.Center).ToRotation() + -MathHelper.PiOver2;
+        }
+
+        public static double GetCosSin(float rot, bool cosSin)
+        {
+            if (cosSin)
+                return Math.Cos(rot);
+            else return Math.Sin(rot);
+        }
+
+        /*
+         *  Charges the Target.
+         *  @Param target: the player the npc is targeting.
+         *  @Param npc: the npc.
+         *  @Param speed: the speed at which to charge. 
+         *  
+         *  Sometimes doesn't work properly, not sure why :s
+         */
+        public static void ChargeTarget(Player target, NPC npc, float speed)
+        {
+            float rot = GetRotation(target, npc, 2);
+
+            npc.velocity.X = (float)Math.Cos(rot) * -speed;
+            npc.velocity.Y = (float)Math.Sin(rot) * -speed;
+        }
+        /*
+         * use this one instead
+         */
+        public static void chargeTarget(CodableEntity codeEntity, Player target, float chargeSpeed)
+        {
+            codeEntity.velocity *= 0.98f;   //slow down
+
+            float distanceX = target.position.X - codeEntity.Center.X;  //get distance from the target
+            float distanceY = target.position.Y - codeEntity.Center.Y;
+
+            float totalDistance = (float) Math.Sqrt(distanceX * distanceX + distanceY * distanceY); //get the total distance
+
+            codeEntity.velocity.X = distanceX * 5f / totalDistance * chargeSpeed;
+            codeEntity.velocity.Y = distanceY * 5f / totalDistance * chargeSpeed;
+        }
+
+        public static void chargePosition(CodableEntity codeEntity, Vector2 position, float chargeSpeed)
+        {
+            float distanceX = position.X - codeEntity.Center.X;  //get distance from the target
+            float distanceY = position.Y - codeEntity.Center.Y;
+
+            float totalDistance = (float)Math.Sqrt(distanceX * distanceX + distanceY * distanceY); //get the total distance
+
+            codeEntity.velocity.X = distanceX * 5f / totalDistance * chargeSpeed;
+            codeEntity.velocity.Y = distanceY * 5f / totalDistance * chargeSpeed;
+        }
+
+        /*  Checks if an npc is in a certain range of the player.
+         *  @Param target: The target of the npc
+         *  @Param npc: the npc itsself.
+         *  @Param range: the distance you want to check.
+         *  @Param inout: whether or not to check if the target is inside the range our outside the range.
+         */
+        public static bool IsInRange(Player target, CodableEntity npc, int range, bool insideRange)
+        {
+            if (insideRange)
+            {
+                if ((npc.Center - target.Center).Length() < range)
+                    return true;
+            }
+            if (!insideRange)
+            {
+                if ((npc.Center - target.Center).Length() > range)
+                    return true;
+            }
+
+            return false;
+        }
+
+        /*
+         *  Slows down the npc.
+         */
+        public static void SlowDown(CodableEntity codeEnt, float speed = 0.98f)
+        {
+            codeEnt.velocity.X *= speed;
+            codeEnt.velocity.Y *= speed;
+        }
+
+        public static void MoveToLocation(CodableEntity codeEntity, Vector2 location, float movement = 0.07f, float mult = 6f)  //copied mostly from terraria's source code, EoC
+        {
+            float distX = location.X - codeEntity.Center.X;
+            float distY = location.Y - codeEntity.Center.Y;
+            float distTotal = (float)System.Math.Sqrt((double)(distX * distX + distY * distY));
+
+            distTotal = mult / distTotal;
+            distX *= distTotal;
+            distY *= distTotal;
+
+            if (codeEntity.velocity.X < distX)
+            {
+                codeEntity.velocity.X += movement;
+                if (codeEntity.velocity.X < 0f && distX > 0f)
+                {
+                    codeEntity.velocity.X += movement;
+                }
+            }
+            else
+            {
+                if (codeEntity.velocity.X > distX)
+                {
+                    codeEntity.velocity.X -= movement;
+                    if (codeEntity.velocity.X > 0f && distX < 0f)
+                    {
+                        codeEntity.velocity.X -= movement;
+                    }
+                }
+            }
+            if (codeEntity.velocity.Y < distY)
+            {
+                codeEntity.velocity.Y += movement;
+                if (codeEntity.velocity.Y < 0f && distY > 0f)
+                {
+                    codeEntity.velocity.Y += movement;
+                }
+            }
+            else
+            {
+                if (codeEntity.velocity.Y > distY)
+                {
+                    codeEntity.velocity.Y -= movement;
+                    if (codeEntity.velocity.Y > 0f && distY < 0f)
+                    {
+                        codeEntity.velocity.Y -= movement;
+                    }
+                }
+            }
+        }
+
+        public static void SlowDown(Projectile proj, float speed = 0.98f)
+        {
+            proj.velocity.X *= speed;
+            proj.velocity.Y *= speed;
+        }
+
+        /*
+         *  Kills the npc if it's active and its life is greater than 0.
+         */
+        public static void SetDead(NPC npc)
+        {
+            if (npc.active && npc.life > 0)
+            {
+                npc.active = false;
+                npc.checkDead();
+            }
+        }
+    }
+}
---
+++ NPCs/Bosses/BossHandOne.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xna.Framework;
+
+using TAPI;
+using Terraria;
+
+namespace MysticalMagics.NPCs
+{
+    class BossHandOne : ModNPC
+    {
+        float maxSpeed = 4;
+        bool lookAtPlayer = false;
+        Vector2 restingLocation;
+        bool moveToRestingLoc;
+        bool justCharged;
+        bool[] attackType = new bool[4];
+        int[] attackTypeTimer = new int[4];
+        bool flag = false;
+        bool spawn = false; //has onspawn stuff been taken care of yet
+
+        public override void AI()
+        {
+            if (!spawn)
+            {   //whenever it spawns, does stuff
+                NPC.NewNPC(npc.Center,NPCDef.byName["MysticalMagics:BossHandTwo"].type, 0); //spawns its brother
+                attackType[0] = true;//sets its attack type to 0 (Flies above the player to the left)
+                spawn = true;   //reset so it doesn't run again
+            }
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                /*if (Main.npc[i].type == NPCDef.byName["MysticalMagics:BossHandTwo"].type)
+                    if (Main.npc[i].active)
+                    {
+                        npc.realLife = Main.npc[i].life;
+                        npc.life = npc.realLife;
+                    }*/
+            }
+
+            if (npc.target < 0 || npc.target == 255 || Main.player[npc.target].dead || !Main.player[npc.target].active)
+                npc.TargetClosest(true);
+            Player target = Main.player[npc.target];
+
+            if (lookAtPlayer)
+                npc.rotation = Helper.GetRotation(target, npc, 2);
+
+            npc.ai[0]++;
+            //Main.NewText("" + npc.ai[0]);
+
+            if (npc.ai[0] > 600)
+            {
+                ResetAttackTypes();
+                justCharged = false;
+                int i = Main.rand.Next(0, 3);
+                attackType[i] = true;
+                /*switch(Main.rand.Next(0,3))
+                {
+                    case 0: attackType[0] = true; break;
+                    case 1: attackType[1] = true; break;
+                    case 2: attackType[2] = true; break;
+                    case 3: attackType[3] = true; break;
+                }*/
+                npc.ai[0] = 0;
+            }
+
+            if (attackType[0])
+            {
+                restingLocation = new Vector2(target.position.X - 150, target.position.Y - 150);
+                Helper.MoveToLocation(npc, restingLocation, 0.07f, 6f);
+            }
+            if (attackType[1])
+            {
+                attackTypeTimer[1]++;
+
+                restingLocation = new Vector2(target.position.X - 150, target.position.Y);
+                if (attackTypeTimer[1] <= 150)
+                    Helper.MoveToLocation(npc, restingLocation, 0.07f, 6f);
+                else if ((npc.Center - restingLocation).Length() < 50 && attackTypeTimer[1] > 150)
+                {
+                    Helper.chargePosition(npc, new Vector2(target.position.X + 300, target.position.Y), 6f);
+                    justCharged = true;
+                    attackTypeTimer[1] = 0;
+                }
+                else Helper.MoveToLocation(npc, restingLocation, 0.07f, 6f);
+            }
+            if (attackType[2])
+            {
+                attackTypeTimer[2]++;
+
+                restingLocation = new Vector2(target.position.X, target.position.Y - 150);
+
+                if (attackTypeTimer[2] <= 150)
+                    Helper.MoveToLocation(npc, restingLocation, 0.07f, 6f);
+                if ((npc.Center - restingLocation).Length() < 50 && attackTypeTimer[2] > 150)
+                {
+                    Helper.chargeTarget(npc, target, 4f);
+                    justCharged = true;
+                    attackTypeTimer[2] = 0;
+                }
+            }
+            if (attackType[3])
+            {
+                restingLocation = new Vector2(target.position.X, target.position.Y);
+                Helper.MoveToLocation(npc, restingLocation, 0.07f, 8f);
+            }
+
+            if (justCharged)
+            {
+                npc.ai[2]++;
+                if(npc.ai[2] > 30)
+                {
+                    npc.velocity.X = 0;
+                    npc.velocity.Y = 0;
+
+                    justCharged = false;
+
+                    npc.ai[2] = 0;
+                }
+            }
+
+            CheckHalfHP();
+            Lighting.AddLight(npc.Center, Color.Yellow);
+        }
+
+        public bool CheckHalfHP()
+        {
+            bool flag = false;
+
+            if (npc.life < npc.lifeMax / 2 && !flag)
+            {   //if the npc is below half hp, do some stuff
+                npc.damage *= 2;
+                npc.defense *= 2;
+                this.maxSpeed *= 2;
+                flag = true;
+                return true;
+            }
+            return false;
+        }
+
+        public void ResetAttackTypes()
+        {   //resets stuff, obveously.
+            for (int i = 0; i < 4; i++)
+            {
+                attackType[i] = false;
+                attackTypeTimer[i] = 0;
+            }
+        }
+    }
+}
